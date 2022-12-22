13653347e6c7f62c1c28b5345612c0b5b7e5de80
David Przybilla <dav.alejandro@gmail.com>
2022-12-22 21:08:03 +0900

    adding cli

--- cmd/dbtest/dbtest.go
+++ cmd/dbtest/dbtest.go
@@ -2,20 +2,62 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/dav009/dbtest"
+	cli "github.com/urfave/cli/v2"
 )
 
 func main() {
-	m := dbtest.ParseManifest("target/manifest.json")
-	t, err := dbtest.ParseTest("test.json")
-	if err != nil {
-		panic(err)
+
+	app := &cli.App{
+		Name:  "boom",
+		Usage: "make an explosive entrance",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "manifest",
+				Value:    "target/manifest.json",
+				Usage:    "Path to your dbt's manifest.json",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "tests",
+				Value:    "",
+				Usage:    "Path to your folder containing json test definitions",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "output",
+				Value:    "",
+				Usage:    "Path where test queries will be gneerated",
+				Required: true,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			manifestPath := cCtx.String("manifest")
+			testsPath := cCtx.String("tests")
+			fmt.Println("Using Manifest: ", manifestPath)
+			m := dbtest.ParseManifest(manifestPath)
+			fmt.Println("Parsed Manifest! ")
+			fmt.Println("Parsing tests in: ", testsPath)
+			t, err := dbtest.ParseTest(testsPath)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Parsed Tests! ")
+			fmt.Println("Generating Tests...")
+			sqlCode, err := dbtest.GenerateTestSQL(t, m)
+			if err != nil {
+				return err
+			}
+			fmt.Println("")
+			fmt.Println(fmt.Sprintf("%v", sqlCode))
+			return nil
+		},
 	}
-	sqlCode, err := dbtest.GenerateTestSQL(t, m)
-	if err != nil {
-		panic(err)
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
-	fmt.Println("")
-	fmt.Println(fmt.Sprintf("%v", sqlCode))
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/dav009/dbtest
 go 1.18
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/perimeterx/marshmallow v1.1.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/urfave/cli/v2 v2.23.7 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
