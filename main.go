062d6905986ad1bddc184b6c730cddef5df25d75
David Przybilla <dav.alejandro@gmail.com>
2022-12-22 17:06:17 +0900

    asserting output

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"errors"
-	"sort"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -12,12 +11,13 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"strings"
 )
 
 /* Describing Tests as structures */
 type Mock struct {
-	Name     string `json:"name"`
+	Name     string            `json:"name"`
 	Filepath string            `json:"filepath"`
 	Types    map[string]string `json:"types"`
 }
@@ -29,14 +29,14 @@ type Replacement struct {
 }
 
 type Output struct {
-	Name     string `json:"name"`
+	Name string `json:"name"`
 }
 
 type Test struct {
 	Name   string          `json:"name"`
 	Model  string          `json:"model"`
 	Mocks  map[string]Mock `json:"mocks"`
-	Output Mock          `json:"output"`
+	Output Mock            `json:"output"`
 }
 
 /* Manifest parsing */
@@ -64,7 +64,10 @@ type Manifest struct {
 	X       map[string]interface{} `json:"-"`
 }
 
+// returns a Manifest structure out of a .json file
 func parseManifest(path string) Manifest {
+	
+
 	jsonFile, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -78,10 +81,15 @@ func parseManifest(path string) Manifest {
 }
 
 func isModel(nodeKey string) bool {
-	return strings.HasPrefix(nodeKey, "model")  ||   strings.HasPrefix(nodeKey, "seed")
+	return strings.HasPrefix(nodeKey, "model") || strings.HasPrefix(nodeKey, "seed")
 }
 
+	/*
+	   Given a SQL query and a replacement Struct, it applies the recplament on the SQL and returns a new SQL query.   References to a Table are replaced
+	*/
 func Replace(sql string, replacement Replacement) string {
+
+
 	if (replacement == Replacement{}) {
 		return sql
 	}
@@ -96,16 +104,25 @@ func Replace(sql string, replacement Replacement) string {
 	return newSql
 }
 
+	/*
+	   Returns the SQL code for a model.
+	   The returned SQL has replacement for the specified mocks.
+	*/
 func sqlModel(manifest Manifest, nodeKey string, mocks map[string]Mock) (Replacement, error) {
+
 	currentNode := manifest.Nodes[nodeKey]
 	fullname := fmt.Sprintf("`%s`.`%s`.`%s`", currentNode.Database, currentNode.Schema, currentNode.Name)
 	shortname := currentNode.Alias
 
 	// if in mocks return the mock replacment
 	if m, ok := mocks[nodeKey]; ok {
+		mockSql, err := mockToSql(m)
+		if err != nil {
+			return Replacement{}, err
+		}
 		r := Replacement{
 			TableFullName:  fullname,
-			ReplaceSql:     mockToSql(m),
+			ReplaceSql:     mockSql.Sql,
 			TableShortName: shortname}
 		return r, nil
 	}
@@ -132,12 +149,21 @@ func sqlModel(manifest Manifest, nodeKey string, mocks map[string]Mock) (Replace
 	return replacement, nil
 }
 
+	/*
+	   Returns the SQL code for a Source.
+	   The returned SQL has replacement for the specified mocks.
+	*/
 func sqlSource(manifest Manifest, sourceKey string, mocks map[string]Mock) (Replacement, error) {
+
 	source := manifest.Sources[sourceKey]
 	if m, ok := mocks[sourceKey]; ok {
+		mockSql, err := mockToSql(m)
+		if err != nil {
+			return Replacement{}, err
+		}
 		r := Replacement{
 			TableFullName:  source.RelationName,
-			ReplaceSql:     mockToSql(m),
+			ReplaceSql:     mockSql.Sql,
 			TableShortName: source.Name}
 		return r, nil
 	}
@@ -145,7 +171,12 @@ func sqlSource(manifest Manifest, sourceKey string, mocks map[string]Mock) (Repl
 	return Replacement{}, nil //errors.New(fmt.Sprintf("%v not mocked", sourceKey))
 }
 
+	/*
+	   Utility Function, converts a CSV file into a List of dictionaries.
+	   Each row is converted into a dictionary where the keys are columns.
+	*/
 func CSVToMap(reader io.Reader) []map[string]string {
+
 	r := csv.NewReader(reader)
 	rows := []map[string]string{}
 	var header []string
@@ -170,38 +201,74 @@ func CSVToMap(reader io.Reader) []map[string]string {
 	return rows
 }
 
-func mockToSql(m Mock) string {
+	/*
+	   Represents a Mock as SQL
+	*/
+type SQLMock struct {
+
+	Sql     string
+	Columns []string
+}
+
+func mockEntryToSql(columnName string, value string, columnType string) string {
+	if value == "" {
+		value = "null"
+	} else {
+		value = fmt.Sprintf("\"%s\"", value)
+	}
+	if columnType!=""{
+		return fmt.Sprintf("CAST(%s AS %s) AS %s", value, columnType, columnName)
+	}
+
+	return  fmt.Sprintf("%s AS %s", value, columnName)
+	
+}
+
+	/*
+	   Converts a Mock into a SQL statement that we can use in Replacements
+	*/
+func mockToSql(m Mock) (SQLMock, error) {
+
+	allColumns := []string{}
 	file, err := os.Open(m.Filepath)
 	if err != nil {
-		panic(err)
+		return SQLMock{}, err
+
 	}
 	data := CSVToMap(file)
 	var sqlStatements []string
 	for _, row := range data {
+
 		columnsValues := []string{}
 		columns := make([]string, 0)
 		// ordering columns so we can test
-		for k, _ := range  row {
+		for k, _ := range row {
 			columns = append(columns, k)
 		}
 		sort.Strings(columns)
+		if len(allColumns) == 0 {
+			allColumns = columns
+		}
 		for _, column := range columns {
 			value := row[column]
-			if columnType, ok := m.Types[column]; ok {
-				columnsValues = append(columnsValues, fmt.Sprintf("CAST(\"%s\" AS %s) AS %s", value, columnType, column))
-			
-			} else {
-				columnsValues = append(columnsValues, fmt.Sprintf("\"%s\" AS %s", value, column))
-			}
+			columnType := m.Types[column]
+			entry := mockEntryToSql(column, value, columnType)
+			columnsValues = append(columnsValues, entry)
 
 		}
 		statement := fmt.Sprintf("\n SELECT %s", strings.Join(columnsValues, ", "))
 		sqlStatements = append(sqlStatements, statement)
 	}
-	return strings.Join(sqlStatements, "\n UNION ALL \n")
+	return SQLMock{Sql: strings.Join(sqlStatements, "\n UNION ALL \n"), Columns: allColumns}, nil
+
 }
 
+	/*
+	   Returns the SQL code for a model/source.
+	   The SQL retunred code has all mocked models/sources replaced for the data the mocks contained
+	*/
 func sql(manifest Manifest, nodeKey string, mocks map[string]Mock) (Replacement, error) {
+
 	if isModel(nodeKey) {
 		return sqlModel(manifest, nodeKey, mocks)
 	} else {
@@ -209,13 +276,17 @@ func sql(manifest Manifest, nodeKey string, mocks map[string]Mock) (Replacement,
 	}
 }
 
+	/*
+	   returns a Test structure given a filepath
+	*/
 func parseTest(path string) (Test, error) {
+
 	jsonFile, err := os.Open(path)
 	if err != nil {
 		return Test{}, err
 	}
 	bytes, err := ioutil.ReadAll(jsonFile)
-	if err!=nil{
+	if err != nil {
 		return Test{}, err
 	}
 	test := Test{}
@@ -225,7 +296,11 @@ func parseTest(path string) (Test, error) {
 	return test, nil
 }
 
+	/*
+	   Given a folder returns a list of Test structs
+	*/
 func parseFolder(path string) ([]Test, error) {
+
 	files, err := ioutil.ReadDir(path)
 	tests := []Test{}
 	if err != nil {
@@ -243,40 +318,44 @@ func parseFolder(path string) ([]Test, error) {
 	return []Test{}, nil
 }
 
+	/*
+	   Given the SQL code of a model and an Expected Output mock,
+	   This function returns a SQL  query which asserts that the output table of SQL is equal to the data contained in the mock
+	*/
 func assertSQLCode(sql string, output Mock) (string, error) {
-	assertTableSql := mockToSql(output)
-	return fmt.Sprintf("%s \n  EXCEPT DISTINCT \n SELECT * FROM (%s)", sql, assertTableSql), nil
+
+	mockedSql, err := mockToSql(output)
+	if err != nil {
+		return "", err
+	}
+	columns := strings.Join(mockedSql.Columns, ",")
+	return fmt.Sprintf("SELECT %s FROM( %s ) \n  EXCEPT DISTINCT \n SELECT %s FROM (%s)", columns, sql, columns, mockedSql.Sql), nil
 }
 
-func GenerateTest(t Test, m Manifest) (string, error){
+	/*
+	   Given a Test it generates the SQL code that mocks data, run the needed logic and asserts the output data
+	*/
+
+func GenerateTestSQL(t Test, m Manifest) (string, error) {
+
 	replacement, err := sql(m, t.Model, t.Mocks)
 	if err != nil {
 		return "", nil
 	}
 	sql, err := assertSQLCode(replacement.ReplaceSql, t.Output)
-	if err!=nil{
+	if err != nil {
 		return "", err
 	}
 	return sql, nil
 }
 
-// check two tables in BQ via: EXCEPT DISTINCT
-
 func main() {
 	m := parseManifest("target/manifest.json")
 	t2, err := parseTest("test.json")
-	if err!=nil {
+	if err != nil {
 		panic(err)
 	}
-	/*t := Test{
-		Name:  "dummy_test",
-		Model: "model.data_feeds.liq_evals_by_asset",
-		Mocks: map[string]Mock{
-			"sme": Mock{Name: "mock1"},
-		},
-		Output: Output{Name: "first check"},
-	}*/
-	sqlCode, err := GenerateTest(t2, m)
+	sqlCode, err := GenerateTestSQL(t2, m)
 	if err != nil {
 		panic(err)
 	}
--- main_private_test.go
+++ main_private_test.go
@@ -30,24 +30,26 @@ func TestReplacePreviousAlias(t *testing.T) {
 
 func TestMockToSql(t *testing.T) {
 	m := Mock{Filepath: "sample.csv"}
-	sql := mockToSql(m)
+	mockAsSQl, err := mockToSql(m)
+	assert.Nil(t, err)
 	expectedSQL1 := "SELECT \"something\" AS column1, \"1.0\" AS column2, \"100\" AS column3"
 	expectedSQL2 := "SELECT \"something2\" AS column1, \"2.0\" AS column2, \"200\" AS column3"
 	expectedSQL3 := "SELECT \"something3\" AS column1, \"3.0\" AS column2, \"300\" AS column3"
-	assert.True(t, strings.Contains(sql, expectedSQL1))
-	assert.True(t, strings.Contains(sql, expectedSQL2))
-	assert.True(t, strings.Contains(sql, expectedSQL3))
+	assert.True(t, strings.Contains(mockAsSQl.Sql, expectedSQL1))
+	assert.True(t, strings.Contains(mockAsSQl.Sql, expectedSQL2))
+	assert.True(t, strings.Contains(mockAsSQl.Sql, expectedSQL3))
 }
 
 func TestMockToSqlWithTypes(t *testing.T) {
 	m := Mock{Filepath: "sample.csv", Types: map[string]string{"column2": "INT64"}}
-	sql := mockToSql(m)
+	mockAsSQl, err := mockToSql(m)
+	assert.Nil(t, err)
 	expectedSQL1 := "SELECT \"something\" AS column1, CAST(\"1.0\" AS INT64) AS column2, \"100\" AS column3"
 	expectedSQL2 := "SELECT \"something2\" AS column1, CAST(\"2.0\" AS INT64) AS column2, \"200\" AS column3"
 	expectedSQL3 := "SELECT \"something3\" AS column1, CAST(\"3.0\" AS INT64) AS column2, \"300\" AS column3"
-	assert.True(t, strings.Contains(sql, expectedSQL1))
-	assert.True(t, strings.Contains(sql, expectedSQL2))
-	assert.True(t, strings.Contains(sql, expectedSQL3))
+	assert.True(t, strings.Contains(mockAsSQl.Sql, expectedSQL1))
+	assert.True(t, strings.Contains(mockAsSQl.Sql, expectedSQL2))
+	assert.True(t, strings.Contains(mockAsSQl.Sql, expectedSQL3))
 }
 
 func TestParseJson(t *testing.T) {
